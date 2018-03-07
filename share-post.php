6b9478456f403337b7e29bc5dc391bdb2ca19c09
rezadeveloper73 <33717989+rezadeveloper73@users.noreply.github.com>
2018-03-07 12:15:29 +0330

    Update share-post.php

--- share-post.php
+++ share-post.php
@@ -28,42 +28,42 @@ function reza_share_post(  $facebook =false, $google=false,$twitter=false,$linke
     
 	<ul class="rd-share-post">
  		<?php if( !empty($facebook)){ ?>
-			<li>
+			<li class="rd-share-facebook">
 				<a href="http://www.facebook.com/sharer.php?u=<?php the_permalink() ?>"><img alt="facebook" width="64" height="64" src="<?php echo  plugin_dir_url( __FILE__ ); ?>/images/facebook.jpg"/></a>
 				<a class="rd-facebook rd-share-social" href="http://www.facebook.com/sharer.php?u=<?php the_permalink() ?>"><?php echo esc_html__('Facebook','reza');?></a>
 			</li>
  		<?php } ?> 
         
 		<?php if(  !empty($google)){ ?>
-			<li>
+			<li  class="rd-share-google" >
 				<a href="https://plus.google.com/share?url=<?php the_permalink() ?>"><img src="<?php echo  plugin_dir_url( __FILE__ ); ?>/images/googleplus.jpg" alt="googleplus" width="64" height="64" /></a>
 				<a class="rd-google rd-share-social" href="https://plus.google.com/share?url=<?php the_permalink() ?>"><?php echo esc_html__('GooglePlus','reza');?></a>
 			</li>
  		<?php } ?>
         
 		<?php if( !empty($twitter)){ ?>
-			<li>
+			<li  class="rd-share-twitter">
 				<a href="<?php echo esc_url($url);?>"><img src="<?php echo  plugin_dir_url( __FILE__ ); ?>/images/twitter.jpg" alt="twitter" width="64" height="64"/></a>
 				<a class="rd-twitter rd-share-social" href="<?php echo esc_url($url);?>"><?php echo esc_html__('Twitter','reza');?></a>
 			</li>
  		<?php } ?> 
         
 		<?php if( !empty($linkedin)){ ?>
-			<li>
+			<li class="rd-share-linkedin">
 				<a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink() ?>"><img src="<?php echo  plugin_dir_url( __FILE__ ); ?>/images/linkedin.jpg" alt="linkedin" width="64" height="64"/></a>
 				<a class="rd-linkedin rd-share-social" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink() ?>"><?php echo esc_html__('Linkedin','reza');?></a>
 			</li>
  		<?php } ?>	
         
 		<?php if( !empty($telegram)){ ?>
-			<li>
+			<li class="rd-share-telegram">
 				<a href="https://telegram.me/share/url?url=<?php the_permalink() ?>"><img src="<?php echo  plugin_dir_url( __FILE__ ); ?>/images/telegram.jpg" alt="linkedin" width="64" height="64"/></a>
 				<a class="rd-telegram rd-share-social" href="https://telegram.me/share/url?url=<?php the_permalink() ?>"><?php echo esc_html__('Telegram','reza');?></a>
 			</li>
  		<?php } ?>	
 
 		<?php if( !empty($whatsapp)){ ?>
-			<li class="rd-whats">
+			<li class=" rd-share-whats rd-whats">
 				<a href="whatsapp://<?php echo esc_attr($title.' '.$per)  ?>"><img src="<?php echo  plugin_dir_url( __FILE__ ); ?>/images/whatsapp.jpg" alt="linkedin" width="64" height="64"/></a>
 				<a class="rd-whatsapp rd-share-social" href="whatsapp://<?php echo esc_attr($title.' '.$per) ?>"><?php echo esc_html__('WhatsApp','reza');?></a>
 			</li>
